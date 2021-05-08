e9361c996fa71a48650ecd97b97820a4386a67a6
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2021-05-08 23:38:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -152,7 +152,7 @@ function commandContact(contactCommand) {
             email: email,
             msg: msg
         }
-        Pageclip.send('yLsaL2m8bDDFfAyVwIOfm9wZ8HYnNxB9', 'contact-form', data, function (error, response) {
+        Pageclip.send('CDEksIa0PVV07owxee5pCi41veILZF2Q/contact-form', 'contact-form', data, function (error, response) {
             if(error) {
                 alert('something went wrong!');
                 console.log('error');
