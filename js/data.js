7899d8f7e9901ffa807e51d11301db36d1c83207
Aniket Sharma <tpbc1717@outlook.com>
2022-11-28 23:57:05 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 var data = {
   name : 'Aniket Sharma',
   bio: [
-    {name: 'About', value: 'Hello 👋! I’m Kamlesh Joshi from India 🇮🇳, a student at Bipin Tripathi Kumaon Institute of Technology,Dwarahat.<br>I love creating useless things and automating daily tasks, I have a decent knowledge of c, c++, python, MySQL, git, java, blockchain technology.<br>I love  ubuntu, and its terminal.<br> Still Learning and Growing.!'}
+    {name: 'About', value: 'Hello 👋! I’m Aniket Sharma from India 🇮🇳, a student at Bipin Tripathi Kumaon Institute of Technology,Dwarahat.<br>I love creating useless things and automating daily tasks, I have a decent knowledge of c, c++, python, MySQL, git, java, blockchain technology.<br>I love  ubuntu, and its terminal.<br> Still Learning and Growing.!'}
   ],
   github: 'Geek-Tekina',
   socials: [
